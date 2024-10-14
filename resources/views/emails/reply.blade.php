222428c841a2444abc1d8ce40d70de5b721f11dc
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2024-10-14 22:47:33 +0800

    Update reply.blade.php

--- resources/views/emails/reply.blade.php
+++ resources/views/emails/reply.blade.php
@@ -2,15 +2,15 @@
 # Thank you for contacting us!
 <br>
 Dear {{ $user->fname }} {{ $user->lname}},
-<br>
+
 Thank you for reaching out to us! We have received your message, and we appreciate your interest in our products and services. We strive to respond to all inquiries within 24 hours, and we will get back to you as soon as possible.
-<br>
+
 However, if you require immediate assistance, do not hesitate to call our helpline <a href="tel:+35315259775">+353 1525 9775</a>.
-<br>
+
 Please note that this is an automated email response. If you have any further questions or concerns, feel free to reply to this email, and one of our representatives will get back to you shortly.
-<br>
+
 Thank you for your patience, and we hope you have a great day.
-<br>
+
 Best regards,  
 <strong>TD ICAV</strong>
 
