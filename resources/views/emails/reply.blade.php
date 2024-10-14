4641936a5d538812b7b144e78d53c2e8742a86ae
quantumexchange8 <96815929+quantumexchange8@users.noreply.github.com>
2024-10-14 22:53:34 +0800

    Update reply.blade.php

--- resources/views/emails/reply.blade.php
+++ resources/views/emails/reply.blade.php
@@ -1,15 +1,15 @@
 @component('mail::message')
-# Thank you for contacting us!\n
+# Thank you for contacting us!
 
-Dear {{ $user->fname }} {{ $user->lname}},\n
+Dear {{ $user->fname }} {{ $user->lname}},
 
-Thank you for reaching out to us! We have received your message, and we appreciate your interest in our products and services. We strive to respond to all inquiries within 24 hours, and we will get back to you as soon as possible.\n
+Thank you for reaching out to us! We have received your message, and we appreciate your interest in our products and services. We strive to respond to all inquiries within 24 hours, and we will get back to you as soon as possible.
 
-However, if you require immediate assistance, do not hesitate to call our helpline <a href="tel:+35315259775">+353 1525 9775</a>.\n
+However, if you require immediate assistance, do not hesitate to call our helpline <a href="tel:+35315259775">+353 1525 9775</a>.
 
-Please note that this is an automated email response. If you have any further questions or concerns, feel free to reply to this email, and one of our representatives will get back to you shortly.\n
+Please note that this is an automated email response. If you have any further questions or concerns, feel free to reply to this email, and one of our representatives will get back to you shortly.
 
-Thank you for your patience, and we hope you have a great day.\n\n
+Thank you for your patience, and we hope you have a great day.
 
 Best regards,  
 <strong>TD ICAV</strong>
